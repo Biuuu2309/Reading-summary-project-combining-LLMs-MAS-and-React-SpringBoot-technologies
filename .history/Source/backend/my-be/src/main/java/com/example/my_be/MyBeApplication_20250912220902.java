18983815e5e5d5239350647e7c23caf7f9a8bb61
Biuuu2309 <148984066+Biuuu2309@users.noreply.github.com>
2025-09-14 12:32:44 +0700

    update

---
+++ .history/Source/backend/my-be/src/main/java/com/example/my_be/MyBeApplication_20250912220902.java
@@ -0,0 +1,13 @@
+package com.example.my_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBeApplication.class, args);
+	}
+
+}
